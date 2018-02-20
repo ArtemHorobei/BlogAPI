5f7eaa17d992dc322d8968a283495c0934e4fbe9
Gorobey Artem <artemgo@123softawre.ru>
2018-02-20 09:56:22 +0200

    Add auth methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
-
   mount_devise_token_auth_for 'User', at: 'auth', controllers: {
       omniauth_callbacks: 'users/omniauth_callbacks',
       registrations:      'users/registrations',
@@ -8,10 +6,8 @@ Rails.application.routes.draw do
       token_validations:  'users/token_validations',
       passwords:          'users/passwords'
   }
-
   devise_scope :user do
     post 'auth/keep_alive', to: 'users/sessions#keep_alive'
     post 'auth/sign_in_social/:provider', to: 'users/sessions#sign_in_social'
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
