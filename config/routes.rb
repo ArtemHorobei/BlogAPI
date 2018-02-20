e3a5429b018287784bd7b752e7168cd5ae71ea9e
Gorobey Artem <artemgo@123softawre.ru>
2018-02-20 17:14:59 +0200

    Add auth methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth', controllers: {
-      omniauth_callbacks: 'users/omniauth_callbacks',
-      registrations:      'users/registrations',
-      sessions:           'users/sessions',
-      token_validations:  'users/token_validations',
-      passwords:          'users/passwords'
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    registrations:      'users/registrations',
+    sessions:           'users/sessions',
+    token_validations:  'users/token_validations',
+    passwords:          'users/passwords'
   }
   devise_scope :user do
     post 'auth/keep_alive', to: 'users/sessions#keep_alive'
